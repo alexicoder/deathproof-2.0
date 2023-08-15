ab8da7dedc1a1012be247af367f1072e35521cc5
alexicoder <alexicoder@gmail.com>
2023-08-15 23:11:37 +0100

    sounds are working and playing on click in sidebar- will experiment with different sound another time

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,34 @@
 const hamburgerButton = document.getElementById('hamburger-button');
 const sidebar = document.getElementById('sidebar');
+const navLinks = document.querySelectorAll('.sidebar-nav-link');
+const socialLinks = document.querySelectorAll('.sidebar-social-icon');
 
 hamburgerButton.addEventListener('click', function () {
   sidebar.classList.toggle('show');
   hamburgerButton.classList.toggle('open');
 });
+
+function playSound(soundSrc) {
+  const sound = new Audio(soundSrc);
+  sound.currentTime = 0; // Reset audio to the beginning
+  sound.play();
+}
+
+navLinks.forEach((link) => {
+  const soundSrc = link.getAttribute('data-sound');
+
+  link.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent the default link behavior
+    playSound(soundSrc);
+  });
+});
+
+socialLinks.forEach((link) => {
+  const soundSrc = link.getAttribute('data-sound');
+
+  link.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent the default link behavior
+    playSound(soundSrc);
+  });
+});
+
